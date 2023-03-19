85402c035fde7928918468595e60264729903b65
Vlad Vobiachenko <109553178+vladosi1@users.noreply.github.com>
2023-03-19 23:14:38 +0200

    dz_16/ first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const generateKey = (length, characters) => {
+	let result = '';
+	for (let i = 0; i < length; i++) {
+	  result += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return result;
+ }
+ 
+
+const key = generateKey(16, characters);
+console.log(key); // For example - yanbc8scq60jsbpp
